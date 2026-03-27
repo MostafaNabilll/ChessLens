4f4f2450643fc122057e5f796f7b158b52a1cf4f
Mostafa Nabil <60539423+MostafaNabilll@users.noreply.github.com>
2026-03-27 23:40:30 +0100

    feat: add gold_time_of_day model

---
+++ dbt_chesslens/models/gold/gold_time_of_day.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized = 'table',
+        schema = 'gold'
+        )
+}}
+
+SELECT
+    EXTRACT(HOUR FROM end_at) AS hour_of_day,
+    DAYOFWEEK(end_at) AS day_of_week,
+    CASE
+        WHEN EXTRACT(HOUR FROM end_at) BETWEEN 6 and 11 THEN 'morning'
+        WHEN EXTRACT(HOUR FROM end_at) BETWEEN 12 and 17 THEN 'afternoon'
+        WHEN EXTRACT(HOUR FROM end_at) BETWEEN 18 and 23 THEN 'evening'
+        ELSE 'night'
+    END as hour_bucket,
+    time_class,
+    COUNT(*) AS games_played,
+    AVG(CASE WHEN result = 'win' THEN 1 ELSE 0 END) AS win_rate,
+    AVG(rating_diff) AS avg_rating_delta
+
+FROM {{ ref('silver_games') }}
+GROUP BY 1, 2, 3, 4
